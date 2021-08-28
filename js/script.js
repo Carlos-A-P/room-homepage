479c5f154f7961a990b5cd44279cb718f55c5762
Carlos-A-P <pcarlos491@gmail.com>
2021-08-27 17:37:12 -0700

    Finished most of the styleing that I wanted to do in both mobile and desktop view. All that left is to finished the Javascript component for the slideshow
    This was the first time I added some animations to my CSS. It was a lot of fun and really helped my understand how some animations/ transitions work. What I hope to do next time is complete the slideshow component but I hope I can add some smooth transition to a background image in my header or else I'm going to have to give it a default look.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hiddenNav = document.getElementById("hidden-nav")
+const blackBackground = document.getElementById("black-background")
+
+// display mobile nav
+function mobileNav() {
+    hiddenNav.classList.toggle("hidden")
+    blackBackground.classList.toggle("hidden")
+}
