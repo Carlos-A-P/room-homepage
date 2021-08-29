82d78af3896a7d890eb9dd68c44a329b91d77a98
Carlos-A-P <pcarlos491@gmail.com>
2021-08-29 15:05:31 -0700

    Finished adding the finishing touches to my project

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const blackBackground = document.getElementById("black-background")
 const mobileHeader = document.getElementById("mobile-header")
 const desktopHeader = document.getElementById("desktop-header")
 let slideIndex = 1
+let buttonClicked = false
 showSlides(slideIndex)
 introAnimation()
 
 // display mobile nav
 function mobileNav() {
-    hiddenNav.classList.toggle("hidden")
+    hiddenNav.classList.toggle("drop-down")
     blackBackground.classList.toggle("visible")
     blackBackground.classList.toggle("hidden")
 }
@@ -27,7 +28,8 @@ function introAnimation(){
 
 // next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n)
+    let buttonClicked = true
+    showSlides(slideIndex += n, buttonClicked)
     slideAnimation()
 }
 
@@ -45,7 +47,7 @@ function slideAnimation() {
 }
 
 
-function showSlides(n) {
+function showSlides(n, buttonActive) {
     const slides = document.getElementsByClassName("slide")
     // if slide on first one
     if (n > slides.length){
@@ -55,19 +57,24 @@ function showSlides(n) {
         slideIndex = slides.length
     }
     
-    
-    setTimeout(()=>{
-        if(slideIndex === 1) {
-            mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-1.jpg)"
-            desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-1.jpg)"
-        } else if (slideIndex === 2) {
-            mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-2.jpg)"
-            desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-2.jpg)"
-        } else {
-            mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-3.jpg)"
-            desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-3.jpg)"
-        }        
-    }, 1500)      
+    if(buttonActive){
+        setTimeout(()=>{
+            if(slideIndex === 1) {
+                mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-1.jpg)"
+                desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-1.jpg)"
+            } else if (slideIndex === 2) {
+                mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-2.jpg)"
+                desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-2.jpg)"
+            } else {
+                mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-3.jpg)"
+                desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-3.jpg)"
+            }        
+        }, 1500) 
+    } else {
+        mobileHeader.style.backgroundImage = "url(../images/mobile-image-hero-1.jpg)"
+        desktopHeader.style.backgroundImage = "url(../images/desktop-image-hero-1.jpg)"
+    }
+         
 
     for(let i = 0; i<slides.length; i++){
         slides[i].style.display = "none"
